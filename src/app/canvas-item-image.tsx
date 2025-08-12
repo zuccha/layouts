44634bbf26c08c77bf7b0a70ffeb49f97e309989
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-12 23:46:04 +0200

    Implement canvas item image

--- src/app/canvas-item-image.tsx
+++ src/app/canvas-item-image.tsx
@@ -1,4 +1,8 @@
 import type { Data } from "@dnd-kit/core";
+import { Image } from "react-konva";
+import useImage from "use-image";
+import useImageUrl from "../hooks/use-image-url";
+import useInterpolatedText from "../hooks/use-interpolated-text";
 import type { LayoutItemImage } from "../models/layout";
 import CanvasItemBox from "./canvas-item-box";
 
@@ -7,6 +11,14 @@ export type CanvasItemImageProps = {
   item: LayoutItemImage;
 };
 
-export default function CanvasItemImage({ item }: CanvasItemImageProps) {
-  return <CanvasItemBox item={item} />;
+export default function CanvasItemImage({ data, item }: CanvasItemImageProps) {
+  const source = useInterpolatedText(item.source, data);
+  const url = useImageUrl(source);
+  const [image] = useImage(url ?? "");
+
+  return (
+    <CanvasItemBox item={item}>
+      {(w, h) => <Image height={h} image={image} width={w} />}
+    </CanvasItemBox>
+  );
 }
