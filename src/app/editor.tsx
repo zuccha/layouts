96342e1c9b8bbb55423bab772a7f72508909191f
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-13 10:37:02 +0200

    Add zoom combobox

--- src/app/editor.tsx
+++ src/app/editor.tsx
@@ -1,11 +1,13 @@
 import { HStack, VStack } from "@chakra-ui/react";
-import { useActiveLayoutSelectedItemId } from "../app-store";
+import { useActiveLayoutSelectedItemId, useCanvasScale } from "../app-store";
+import Combobox from "../components/ui/combo-box";
 import { sidebarWidth } from "../theme/theme-constants";
 import EditorExportButton from "./editor-export-button";
 import EditorItemTab from "./editor-item-tab";
 
 export default function Editor() {
   const itemId = useActiveLayoutSelectedItemId();
+  const [scale, setScale] = useCanvasScale();
 
   return (
     <VStack
@@ -19,11 +21,23 @@ export default function Editor() {
       <HStack
         align="center"
         borderBottomWidth={1}
-        justify="flex-end"
+        justify="space-between"
         minH="3em"
         px={4}
         w="100%"
       >
+        <Combobox
+          onValueChange={(scaleStr) => {
+            const nextScale = parseFloat(scaleStr) / 100;
+            setScale(isNaN(nextScale) ? 1 : nextScale);
+          }}
+          options={zoomOptions}
+          placeholder="Zoom"
+          size="xs"
+          value={`${Math.round(scale * 100)}%`}
+          w="4.5em"
+        />
+
         <EditorExportButton />
       </HStack>
 
@@ -31,3 +45,5 @@ export default function Editor() {
     </VStack>
   );
 }
+
+const zoomOptions = ["50%", "75%", "100%", "150%", "200%"];
